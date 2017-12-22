5f22910d4a0c161ac92bb2ca5b9bf11f8fbc13a7
gaomingqi <gmingqi1993@foxmail.com>
2017-12-23 04:08:22 +0800

    update FACM

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # FACM
-## Introduction
-This project is a MATLAB implementation for the texture segmentation method in paper:   
-"A Factorization based Active Contour Model for Texture Segmentation", Mingqi Gao, Hengxin Chen, Shenhai Zheng, Bin Fang, IEEE International Conference on Image Processing (ICIP 2016).
+A MATLAB implementation for the texture segmentation method in paper:   
+>[A factorization based active contour model for texture segmentation](http://ieeexplore.ieee.org/document/7533173/), Mingqi Gao, Hengxin Chen, Shenhai Zheng, Bin Fang, IEEE International Conference on Image Processing (ICIP 2016).    
+
+To use FACM as academic purpose, please cite this paper. Some details about it can be found in [my blog](https://gaomingqi.github.io/posts/2017/04/blog-post-3/).
+
 ## Statement
--- In this project, the extraction of texture features from input image is based on the original code provided by Jiangye Yuan, Deliang Wang et al:  
-[1] Yuan J, Wang D, Cheriyadat A M. Factorization-Based Texture Segmentation.[J]. IEEE Transactions on Image Processing, 2015, 24(11):3488-97.   
--- The penalty term of level set function is implemented based on Chunming Li's paper:  
-[2] Li C, Xu C, Gui C, et al. Level Set Evolution without Re-Initialization: A New Variational Formulation.[C]. IEEE Conference on Computer Vision and Pattern Recognition, 2005. 430-436.
+1. In this project, the extraction of texture features from given image is based on the theory proposed by Jiangye Yuan, Deliang Wang et al:  
+[Factorization-Based Texture Segmentation](http://ieeexplore.ieee.org/document/7127013/). Jiangye Yuan, Deliang Wang, and Anil M Cheriyadat. IEEE Transactions on Image Processing, 2015.   
+2. The penalty term of energy functional is implemented based on Chunming Li's paper:  
+[Level Set Evolution without Re-Initialization: A New Variational Formulation](http://ieeexplore.ieee.org/document/1467299/). Chunming Li, Chenyang Xu, Changfeng Gui, and Martin D. Fox. IEEE CVPR, 2005.
+
 ## Usage
-Test texture images are located in "images" folder.  
-Run "main_FACM.m" to get segmentation results.
+* The test images are placed in "images" folder;
+* You can load the existing initial contours from "initial contours" folder or select a polygonal region by hand;
+* Also, you can record the evolution process of active contours in a GIF file;
+* Segmentation results will be saved in "results" folder;
+* __Run "main_FACM.m" directly to get segmentation results!__
--- main_FACM.m
+++ main_FACM.m
@@ -1,8 +1,7 @@
 % This project demonstrates the texture segmentation method in paper:
 %   "A factorization based active contour model for texture segmentation", 
 %   Mingqi Gao, Hengxin Chen, Shenhai Zheng, Bin Fang, 
-%   IEEE International Conference on Image Processing, pp 4309-4313, 2016.
-
+%   IEEE International Conference on Image Processing (ICIP), 2016.
 % Main References in our project: 
 % [1] Yuan J, Wang D, Cheriyadat A M, 
 %    "Factorization based Texture Segmentation",
@@ -12,46 +11,46 @@
 %    IEEE Conference on Computer Vision and Pattern Recognition, pp.430-436, 2005.
 clc, clear all, close all;
 
-I = imread('images/S1_x.jpg');
-if size(I, 3) > 1
-    I = rgb2gray(I);
+u0 = imread('images/S2.jpg');
+if size(u0, 3) > 1
+    u0 = rgb2gray(u0);          % convert given image into grey-level
 end
-I = double(I);
+u0 = double(u0);
 
 %% Coefficient setting
-maxIter = 50;    % Maximum count of iteration process. 
-mu = 1;          % Coefficient of data term.
+maxIter = 40;    % Maximum count of iteration process. 
+mu = 1;          % Coefficient of fitting term.
 epsilon = 1;
 nu = 1;          % Coefficient of penalty term with respect to level set function,
 % used to make sure the updated level set function can be close enough to
 % signal distance function.  
 timeStep = .1;   % Time step.
-c0 = 2;
 bins = 12;
-windowsize = 15;      % Window size during computing Spectrum histograms.
+windowsize = 20;      % Window size during computing Spectrum histograms.
 % Note: a higher window-size indicates a more smooth segmentation result.
 
-% Initialize signed distance function phi. 
-initLSF = ones(size(I)).*c0;
-[row, col] = size(I);
-initLSF(:, floor(col/3)) = -c0;
-initLSF(:, floor(col*2/3)) = -c0;
-initLSF(floor(row/3), :) = -c0;
-initLSF(floor(row*2/3), :) = -c0;
-phi = initLSF;
+% initialize level set function or load a existing initial contour
+%--- if you want initialize the active contour by Manual setting, please
+%remove the following comments:
+% m = roipoly;  % select a polygonal region by hand;
+% phi = bwdist(m) - bwdist(1-m) + im2double(m) - .5;  % compute SDF according to m;
+%--- Load existing initial contour
+load('initial contours/init_contour1.mat');  % circle contour
+% load('initial contours/init_contour2.mat');    % 9 even-distributed retangle contours
+phi = imresize(phi, size(u0));
 
 % Show input image and initial contour.
-subplot(1, 2, 1), imshow(I, []);
-hold on, contour(phi, [0 0], 'r', 'linewidth', 2);
+subplot(1, 2, 1), imshow(u0, []);
+hold on, contour(phi, [0.5 0.5], 'r', 'linewidth', 2);
 % In this project, evolving contour is considerd as the zero level set of
 % signed distance function phi. 
-title('Input image');
+title('Given image');
 hold off;
 
 %% Compute texture features using spectrul histograms [1]
-F = compFeaturemaps(I);
-H = compHistogram(F, windowsize, bins); 
-H = double(H);
+F = compFeaturemaps(u0);            % compute feature maps from given image
+H = compHistograms(F, windowsize);  % compute feature histograms of each pixel within fixed window
+H = double(H);                      
 
 %% Drive active contour move towards true region boundaries by 
 %  updating level set function phi. 
@@ -61,13 +60,33 @@ for i = 1:maxIter
     % Show current active contour every 5 iterations
     if mod(i, 5) == 0 || i == 1
         pause(.1);
-        subplot(1, 2, 2), imshow(I, []), hold on;
+        subplot(1, 2, 2), imshow(u0, []), hold on;
         contour(phi, [0 0], 'r', 'linewidth', 2);
   
         % Display the iteration number. 
         iterNum=[num2str(i), ' iterations'];
         title(iterNum);
         hold off;
+%--- if you want to save the segmentation process as a GIF file, please
+%    remove the following comments:
+%         frame = getframe(gcf);
+%         im = frame2im(frame);   % extract image from current figure;
+%         [I, map] = rgb2ind(im,256);
+% 
+%         if i==1;
+%              imwrite(I, map, 'results/evo_process.gif', 'gif', 'Loopcount', inf,...
+%                  'DelayTime', 0.3);  % initialize GIF file;
+%         else
+%             imwrite(I, map, 'results/evo_process.gif', 'gif', 'WriteMode', 'append',...
+%                 'DelayTime', 0.2);  % add new frame every each 5 iterations;
+%         end
+%--- 
     end
 end
 
+%% save the mask of segmentation result;
+Iseg = zeros(size(phi));
+Iseg(phi > 0) = 1;
+imwrite(Iseg, 'results/seg_result.jpg');
+
+disp('end of FACM seg program');
